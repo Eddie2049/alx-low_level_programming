6e8203cd08231262b383af32b605504d3df44b41
ed_ <eddieweks@gmail.com>
2023-10-05 18:59:55 +0300

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * program prints an exact output. 
- * uses puts function. 
+ * program prints an exact output
+ * uses puts function
  * ends with a value of 0
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main: entry point 
+ * main: entry point
  * program prints specific message
- * only uses printf 
+ * only uses printf
  * return: always 0
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
