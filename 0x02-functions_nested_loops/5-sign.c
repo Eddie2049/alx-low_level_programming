f34de867ff222fddd83a33d8ae3b2fa8f599eb8c
ed_ <eddieweks@gmail.com>
2023-10-12 01:03:05 +0300

    updates

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "5-sign.h"
+#include "_putchar.h"
+
+/**
+ * print_sign - prints sign of an int
+ *
+ * @n: int to check
+ * Return: Always 0 | 1 | -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('+');
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
