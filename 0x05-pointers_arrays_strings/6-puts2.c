49aef9b0e8c2a89fa6e9ec91b7fccfcd877fb91f
ed_ <eddieweks@gmail.com>
2023-10-17 02:35:53 +0300

    updates

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,12 @@ void puts2(char *str)
 {
 	int i, j, len;
 
+	len = 0;
+
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 
-	for (j = 0; j < len; j++)
+	for (j = 0; j < len; ++j)
 	{
 		if (j % 2 == 0)
 			_putchar(str[j]);
