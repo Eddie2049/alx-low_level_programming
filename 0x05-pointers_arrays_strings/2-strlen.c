e6c435a4d924f94f5d315001a9d7fff77e4ff934
ed_ <eddieweks@gmail.com>
2023-10-16 21:04:16 +0300

    updates

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: sring param
+ * 
+ * Return: int length of string
+*/
+
+int _strlen(char *s)
+{
+	int count;
+	int i;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
