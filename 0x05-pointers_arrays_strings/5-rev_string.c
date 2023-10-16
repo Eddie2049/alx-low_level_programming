aad72f1b7d98ee0e708cd0021db19c751f1618ff
ed_ <eddieweks@gmail.com>
2023-10-17 01:34:06 +0300

    updates

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,18 @@
 void rev_string(char *a)
 {
 	int i, j, len;
-	char s[999];
 
 	len = 0;
 
 	for (i = 0; a[i] != 0; i++)
 		len++;
 
-	for (j = 0; j < len; ++j)
+	for (j = 0; j < len / 2; ++j)
 	{
-		*(s + len - 1 - j) = a[j];
-		a[j] = s[j];
+		char s;
+
+		s = a[j];
+		a[j]= a[len - 1 - j];
+		a[len - 1 - j] = s;
 	}
 }
