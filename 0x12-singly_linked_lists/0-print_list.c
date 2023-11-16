1532832182188fa3d9fb1433734aa1d601244460
ed_ <eddieweks@gmail.com>
2023-11-17 01:27:14 +0300

    updates

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: param.
+ * Return: size_t no. of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t len_nodes;
+
+	len_nodes = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		++len_nodes;
+	}
+	return (len_nodes);
+}
