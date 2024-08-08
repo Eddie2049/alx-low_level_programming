bec2e2ef53aff2ee70130bdf8226246153ebdda9
ed_ <eddieweks@gmail.com>
2024-08-09 01:35:18 +0300

    updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num;
 
 	num = 0;
+	/* check if b is null*/
 	if (!b)
 		return (0);
 
 	for (a = 0; b[a] != '\0'; a++)
 	{
+		/**
+		 * check if there is one or more chars
+		 * in the string b that is not 0 or 1
+		*/
 		if (b[a] != '0' && b[a] != '1')
 			return (0);
 	}
